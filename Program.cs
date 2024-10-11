bf7a9212aeef682e63ba8c3926f9703ba11bdd20
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-10-11 13:25:56 -0300

    feat: add JWT configuration

--- Program.cs
+++ Program.cs
@@ -1,25 +1,57 @@
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TallerIDWMBackend.Data;
 using TallerIDWMBackend.Interfaces;
 using TallerIDWMBackend.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Cargar variables de entorno
 Env.Load();
+
+// Swagger y Controllers
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-//Servicios
+// Conexión a la base de datos
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
-builder.Services.AddScoped<DataSeeder>();
 
+// Inyección de dependencias
+builder.Services.AddScoped<DataSeeder>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+// Configuración JWT
+var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET_KEY")!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, // Opcional: puedes configurarlo si tienes un emisor específico.
+        ValidateAudience = false, // Opcional: puedes configurarlo si tienes una audiencia específica.
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero // Elimina retrasos de expiración del token.
+    };
+});
+
 var app = builder.Build();
+
+// Sembrar datos (Data Seeder)
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -27,13 +59,17 @@ using (var scope = app.Services.CreateScope())
     await seeder.SeedProductsAsync();
     await seeder.SeedPostsAsync();
 }
+
+// Swagger en desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Middleware
 app.UseHttpsRedirection();
+app.UseAuthentication(); // Asegúrate de añadir esta línea para usar JWT
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
