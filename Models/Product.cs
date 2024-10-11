848344cf77e1e478ba60952d9e7a6811688e8660
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-10-11 16:09:26 -0300

    chore: change id type

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,7 +8,7 @@ namespace TallerIDWMBackend.Models
     public class Product
     {
         [Key]
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         [Required]
         [StringLength(64, MinimumLength = 10, ErrorMessage = "El nombre debe tener entre 10 y 64 caracteres.")]
