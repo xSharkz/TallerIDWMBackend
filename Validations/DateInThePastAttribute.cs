09014df60412892133b8dcb290ec16f046396132
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-11-24 03:16:14 -0300

    docs: add validators documentation.

--- Validations/DateInThePastAttribute.cs
+++ Validations/DateInThePastAttribute.cs
@@ -5,13 +5,30 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 namespace TallerIDWMBackend.Validation
 {
+    /// <summary>
+    /// Atributo de validación personalizado que verifica si una fecha es anterior a la fecha actual.
+    /// </summary>
     public class DateInThePastAttribute : ValidationAttribute
     {
+        /// <summary>
+        /// Inicializa una nueva instancia del atributo <see cref="DateInThePastAttribute"/> y establece el mensaje de error predeterminado.
+        /// </summary>
         public DateInThePastAttribute()
         {
             ErrorMessage = "La fecha debe ser anterior a la fecha actual.";
         }
 
+        /// <summary>
+        /// Valida si el valor proporcionado es una fecha que es anterior a la fecha actual.
+        /// </summary>
+        /// <param name="value">El valor a validar. Debe ser un objeto de tipo <see cref="DateTime"/>.</param>
+        /// <param name="validationContext">El contexto de validación que contiene información sobre el objeto que se está validando.</param>
+        /// <returns>Devuelve un <see cref="ValidationResult"/> que indica si la validación fue exitosa o no.</returns>
+        /// <remarks>
+        /// Si el valor es nulo, la validación se considera exitosa.
+        /// Si el valor es un objeto <see cref="DateTime"/>, se verifica si la fecha es anterior a la fecha actual.
+        /// En caso contrario, se devuelve un mensaje de error que indica que el valor no es una fecha válida.
+        /// </remarks>
         protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
         {
             if (value == null)
--- Validations/RutValidatorAttribute.cs
+++ Validations/RutValidatorAttribute.cs
@@ -4,13 +4,29 @@ using System.Text.RegularExpressions;
 
 namespace TallerIDWMBackend.Validations
 {
+    /// <summary>
+    /// Atributo de validación personalizado que verifica si un RUT chileno es válido.
+    /// </summary>
     public class RutValidatorAttribute : ValidationAttribute
     {
+        /// <summary>
+        /// Inicializa una nueva instancia del atributo <see cref="RutValidatorAttribute"/>.
+        /// Establece el mensaje de error predeterminado.
+        /// </summary>
         public RutValidatorAttribute()
         {
             ErrorMessage = "El rut ingresado no es válido.";
         }
-
+        /// <summary>
+        /// Valida si el RUT proporcionado es válido según las reglas del sistema de RUT chileno.
+        /// </summary>
+        /// <param name="value">El valor a validar, que debe ser un RUT en formato de cadena.</param>
+        /// <param name="validationContext">El contexto de validación que contiene información adicional sobre el objeto que se está validando.</param>
+        /// <returns>Devuelve un <see cref="ValidationResult"/> que indica si la validación fue exitosa o si el RUT es inválido.</returns>
+        /// <remarks>
+        /// El RUT debe tener el formato de 7 u 8 dígitos seguidos de un guion y el dígito verificador.
+        /// Además, se valida que el dígito verificador calculado coincida con el ingresado.
+        /// </remarks>
         protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
         {
             // Comprobar si el valor es nulo
@@ -43,7 +59,14 @@ namespace TallerIDWMBackend.Validations
 
             return new ValidationResult(ErrorMessage); // Si no es válido, devuelve el mensaje de error
         }
-
+        /// <summary>
+        /// Calcula el dígito verificador de un RUT chileno a partir de su número.
+        /// </summary>
+        /// <param name="rutNumbers">El número del RUT, sin el dígito verificador.</param>
+        /// <returns>Devuelve el dígito verificador calculado.</returns>
+        /// <remarks>
+        /// El dígito verificador se calcula utilizando un algoritmo específico basado en los dígitos del RUT y multiplicadores predefinidos.
+        /// </remarks>
         private static int CalculateCheckDigit(string rutNumbers)
         {
             int[] multipliers = new int[] { 2, 3, 4, 5, 6, 7, 2, 3, 4 }; // Los multiplicadores para el cálculo
